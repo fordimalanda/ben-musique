1cc5717a6914057901de02920c9316192ea8cfd5
fordimalanda <fordimalanda7@gmail.com>
2025-08-20 14:22:26 +0100

    Add database configuration file

--- config/db.php
+++ config/db.php
@@ -1,18 +1,16 @@
 <?php
-// config/db.php
-
-$host = 'localhost';         // ou 127.0.0.1
-$db   = 'ben_musique';       // nom de ta base de données
-$user = 'root';              // utilisateur MySQL
-$pass = '';                  // mot de passe MySQL (souvent vide en local)
+$host = 'localhost';
+$db   = 'ben_musique';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Pour afficher les erreurs SQL
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,        // Pour récupérer des tableaux associatifs
-    PDO::ATTR_EMULATE_PREPARES   => false,                   // Pour sécuriser les requêtes préparées
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
